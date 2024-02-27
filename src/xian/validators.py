c5ad6666a88a6f445564a03df986ca28c76d22a2
crosschainer <eodkwokm@gmail.com>
2024-02-27 13:34:14 +0000

    validators synchronized with state masternodes

---
+++ src/xian/validators.py
@@ -0,0 +1,26 @@
+from tendermint.abci.types_pb2 import ValidatorUpdate
+from tendermint.crypto.keys_pb2 import PublicKey
+import requests
+
+class ValidatorHandler():
+    def __init__(self, app):
+        self.driver = app.driver
+        self.app = app
+       
+    def get_validators_from_state(self) -> list[str]:
+        return self.driver.get("masternodes.S:members")
+    
+    def get_tendermint_validators(self) -> list[str]:
+        response = requests.get("http://localhost:26657/validators")
+        return [validator['pub_key']['data'] for validator in response.json()['result']['validators']]
+    
+    def build_validator_updates(self) -> list[ValidatorUpdate]:
+        validators_state = self.get_validators_from_state()
+        validators_tendermint = self.get_tendermint_validators()
+        updates = []
+        for validator in validators_state:
+            if validator not in validators_tendermint:
+                updates.append(ValidatorUpdate(pub_key=PublicKey(ed25519=validator), power=0))
+            else:
+                updates.append(ValidatorUpdate(pub_key=PublicKey(ed25519=validator), power=10))
+        return updates
--- src/xian/xian_abci.py
+++ src/xian/xian_abci.py
@@ -21,6 +21,8 @@ from tendermint.abci.types_pb2 import (
     ResponseCommit,
 )
 
+from xian.validators import ValidatorHandler
+
 from abci.server import ABCIServer
 from abci.application import BaseApplication, OkCode, ErrorCode
 from xian.driver_api import (
@@ -76,6 +78,7 @@ class Xian(BaseApplication):
         self.driver = ContractDriver()
         self.nonce_storage = NonceStorage()
         self.xian = Node(self.client, self.driver, self.nonce_storage)
+        self.validator_handler = ValidatorHandler(self)
         self.current_block_meta: dict = None
         self.fingerprint_hashes = []
         self.chain_id = self.config.get("chain_id", None)
@@ -227,7 +230,7 @@ class Xian(BaseApplication):
         you can use the height from the request to record the last_block_height
         """
 
-        return ResponseEndBlock()
+        return ResponseEndBlock(validator_updates=self.validator_handler.build_validator_updates())
 
     def commit(self) -> ResponseCommit:
         """
