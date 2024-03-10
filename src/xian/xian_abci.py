67e697709b2447da2f24cd2cf13bfaa5aea19ae1
crosschainer <eodkwokm@gmail.com>
2024-03-10 11:41:38 +0000

    bring back sig check

--- src/xian/xian_abci.py
+++ src/xian/xian_abci.py
@@ -151,6 +151,11 @@ class Xian(BaseApplication):
         try:
             tx = decode_transaction_bytes(raw_tx)
             self.xian.validate_transaction(tx)
+            sender, signature, payload = unpack_transaction(tx)
+            if not verify(sender, payload, signature):
+                return ResponseCheckTx(code=ErrorCode)
+            if payload["chain_id"] != self.chain_id:
+                return ResponseCheckTx(code=ErrorCode)
             return ResponseCheckTx(code=OkCode)
         except Exception as e:
             logger.error(e)
