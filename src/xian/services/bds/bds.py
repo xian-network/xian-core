e103ca8c803e03b637f9a80c4d44bbee68092c41
endogen <081081@gmx.net>
2024-06-20 02:02:43 +0200

    Added all SQL sripts and insert tx, state changes and rewards

--- src/xian/services/bds/bds.py
+++ src/xian/services/bds/bds.py
@@ -1,8 +1,19 @@
-from loguru import logger
+import json
 
+from loguru import logger
+from datetime import datetime
 from xian.services.bds import sql
 from xian.services.bds.config import Config
 from xian.services.bds.database import DB
+from contracting.stdlib.bridge.decimal import ContractingDecimal
+
+
+# Custom JSON encoder for ContractingDecimal
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ContractingDecimal):
+            return str(obj)
+        return super().default(obj)
 
 
 class BDS:
@@ -21,3 +32,76 @@ class BDS:
             self.db.execute(sql.create_addresses())
         except Exception as e:
             logger.exception(e)
+
+    def insert_tx(self, tx: dict):
+        try:
+            self.db.execute(sql.insert_transaction(), {
+                'hash': tx['tx_result']['hash'],
+                'contract': tx['payload']['contract'],
+                'function': tx['payload']['function'],
+                'sender': tx['payload']['sender'],
+                'nonce': tx['payload']['nonce'],
+                'stamps': tx['tx_result']['stamps_used'],
+                'block_hash': tx['b_meta']['hash'],
+                'block_height': tx['b_meta']['height'],
+                'block_time': tx['b_meta']['nanos'],
+                'status': tx['tx_result']['status'],
+                'result': tx['tx_result']['result'],
+                'json_content': json.dumps(tx, cls=CustomEncoder),
+                'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            })
+        except Exception as e:
+            logger.exception(e)
+
+    def insert_state_changes(self, tx: dict):
+        for state_change in tx['tx_result']['state'].items():
+            try:
+                self.db.execute(sql.insert_state_changes(), {
+                    'id': None,
+                    'tx_hash': tx['tx_result']['hash'],
+                    'key': state_change['key'],
+                    'value': json.dumps(state_change['value'], cls=CustomEncoder),
+                    'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                })
+            except Exception as e:
+                logger.exception(e)
+
+    def insert_rewards(self, tx: dict):
+        def insert(reward_type, key, value):
+            self.db.execute(sql.insert_rewards(), {
+                'id': None,
+                'tx_hash': tx['tx_result']['hash'],
+                'reward_type': reward_type,
+                'key': key,
+                'value': value,
+                'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            })
+
+        # Developer reward
+        for address, reward in tx['tx_result']['rewards']['developer_rewards'].items():
+            try:
+                insert('developer_rewards', address, reward)
+            except Exception as e:
+                logger.exception(e)
+
+        # Masternode reward
+        for address, reward in tx['tx_result']['rewards']['masternode_reward'].items():
+            try:
+                insert('masternode_reward', address, reward)
+            except Exception as e:
+                logger.exception(e)
+
+        # Foundation reward
+        try:
+            reward_value = tx['tx_result']['rewards']['foundation_reward']
+            insert('foundation_reward', '', reward_value)
+        except Exception as e:
+            logger.exception(e)
+
+    def insert_addresses(self, tx: dict):
+        # TODO: Insert from and to addresses
+        pass
+
+    def insert_contracts(self, tx: dict):
+        # TODO: Insert contracts if contract = 'submission' and function = 'submit_contract'
+        pass
--- src/xian/services/bds/sql.py
+++ src/xian/services/bds/sql.py
@@ -12,7 +12,7 @@ def create_transactions():
         block_time BIGINT NOT NULL,
         status INTEGER NOT NULL,
         result TEXT,
-        data JSONB NOT NULL,
+        json_content JSONB NOT NULL,
         created TIMESTAMP NOT NULL
     )
     """
@@ -35,8 +35,9 @@ def create_rewards():
     CREATE TABLE IF NOT EXISTS rewards (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         tx_hash TEXT REFERENCES transactions(hash),
-        key TEXT NOT NULL,
-        value BIGINT NOT NULL,
+        reward_type TEXT NOT NULL,
+        key TEXT,
+        value DECIMAL NOT NULL,
         created TIMESTAMP NOT NULL
     )
     """
@@ -66,4 +67,88 @@ def create_contracts():
 def select_db_size():
     return """
     SELECT pg_size_pretty(pg_database_size(%(n)s))
+    """
+
+
+def insert_transaction():
+    return """
+    INSERT INTO transactions(
+        hash, contract, function, sender, nonce, stamps, block_hash, 
+        block_height, block_time, status, result, json_content, created)
+    VALUES (
+        %(hash)s, %(contract)s, %(function)s, %(sender)s, %(nonce)s, 
+        %(stamps)s, %(block_hash)s, %(block_height)s, %(block_time)s, 
+        %(status)s, %(result)s, %(json_content)s, %(created)s)
+    ON CONFLICT (hash) DO UPDATE 
+    SET 
+        contract = %(contract)s,
+        function = %(function)s,
+        sender = %(sender)s,
+        nonce = %(nonce)s,
+        stamps = %(stamps)s,
+        block_hash = %(block_hash)s,
+        block_height = %(block_height)s,
+        block_time = %(block_time)s,
+        status = %(status)s,
+        result = %(result)s,
+        json_content = %(json_content)s,
+        created = %(created)s;
+    """
+
+
+def insert_state_changes():
+    return """
+    INSERT INTO state_changes(
+        id, tx_hash, key, value, created)
+    VALUES (
+        COALESCE(%(id)s, gen_random_uuid()), %(tx_hash)s, %(key)s, %(value)s, %(created)s)
+    ON CONFLICT (id) DO UPDATE 
+    SET 
+        tx_hash = %(tx_hash)s,
+        key = %(key)s,
+        value = %(value)s,
+        created = %(created)s;
+    """
+
+
+def insert_rewards():
+    return """
+    INSERT INTO rewards(
+        id, tx_hash, key, value, created)
+    VALUES (
+        COALESCE(%(id)s, gen_random_uuid()), %(tx_hash)s, %(reward_type)s, %(key)s, %(value)s, %(created)s)
+    ON CONFLICT (id) DO UPDATE 
+    SET 
+        tx_hash = %(tx_hash)s,
+        reward_type = %(reward_type)s,
+        key = %(key)s,
+        value = %(value)s,
+        created = %(created)s;
+    """
+
+
+def insert_addresses():
+    return """
+    INSERT INTO addresses(
+        tx_hash, address, created)
+    VALUES (
+        %(tx_hash)s, %(address)s, %(created)s)
+    ON CONFLICT (address) DO UPDATE 
+    SET 
+        tx_hash = %(tx_hash)s,
+        created = %(created)s;
+    """
+
+
+def insert_contracts():
+    return """
+    INSERT INTO contracts(
+        tx_hash, name, code, created)
+    VALUES (
+        %(tx_hash)s, %(name)s, %(code)s, %(created)s)
+    ON CONFLICT (name) DO UPDATE 
+    SET 
+        tx_hash = EXCLUDED.tx_hash,
+        code = EXCLUDED.code,
+        created = EXCLUDED.created;
     """
