d6ed5e588f104be8b9310726fff38aca46791912
endogen <081081@gmx.net>
2024-07-21 15:33:48 +0200

    Made websocket subscriber disconnect scenario more robust

--- src/xian/services/web_sockets.py
+++ src/xian/services/web_sockets.py
@@ -2,6 +2,8 @@ import json
 import asyncio
 import websockets
 
+from loguru import logger
+
 
 class WebSocketServer:
     def __init__(self, bds_instance):
@@ -10,14 +12,17 @@ class WebSocketServer:
 
     async def register(self, websocket):
         self.subscribers.add(websocket)
+        logger.info(f"New subscriber connected: {websocket.remote_address}")
 
     async def unregister(self, websocket):
         self.subscribers.remove(websocket)
+        logger.info(f"Subscriber disconnected: {websocket.remote_address}")
 
     async def notify_subscribers(self, key, value):
         if self.subscribers:
-            message = json.dumps({'key': key, 'value': value}, cls=self.bds_instance.CustomEncoder)
-            await asyncio.wait([ws.send(message) for ws in self.subscribers])
+            message = json.dumps({'key': key, 'value': str(value)})
+            tasks = [asyncio.create_task(ws.send(message)) for ws in self.subscribers]
+            await asyncio.gather(*tasks)
 
     async def websocket_handler(self, websocket, path):
         await self.register(websocket)
@@ -28,6 +33,10 @@ class WebSocketServer:
                 if key:
                     history = await self.bds_instance.get_state_history(key)
                     await websocket.send(history)
+        except websockets.exceptions.ConnectionClosedError:
+            logger.info(f"Connection closed unexpectedly for subscriber: {websocket.remote_address}")
+        except Exception as e:
+            logger.exception(f"Unexpected error for subscriber {websocket.remote_address}: {e}")
         finally:
             await self.unregister(websocket)
 
