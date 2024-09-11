767451a103dfd3e79c1786804adb6676091ffe45
crosschainer <eodkwokm@gmail.com>
2024-09-11 09:46:09 +0000

    fix test and rewards contract not zero

--- src/xian/tools/genesis/contracts/rewards.s.py
+++ src/xian/tools/genesis/contracts/rewards.s.py
@@ -12,4 +12,5 @@ def current_value():
 def set_value(new_value: list):
     assert len(new_value) == 4, 'New value must be a list of 4 elements'
     assert sum(new_value) == 1, 'Sum of new value must be 1'
+    assert all([x > 0 for x in new_value]), 'All elements of new value must be greater'
     S['value'] = new_value
