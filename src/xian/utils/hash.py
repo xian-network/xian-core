85dfc4db4aefa3cf39a0f8bf74a32a02deedbfb4
Endogen <Endogen@users.noreply.github.com>
2024-10-07 09:29:10 +0200

    Removed unused function

--- src/xian/utils/hash.py
+++ src/xian/utils/hash.py
@@ -13,18 +13,3 @@ def hash_from_rewards(rewards):
     encoded_rewards = encode(rewards).encode()
     h.update(encoded_rewards)
     return h.hexdigest()
-
-
-def hash_from_validator_updates(validator_updates):
-    h = hashlib.sha3_256()
-    
-    if not validator_updates:  # Check if the list is empty
-        # Use a consistent value to hash when the list is empty
-        h.update(b'')  # Hash of an empty byte sequence
-    else:
-        # Serialize each validator update using protobuf's SerializeToString() method
-        for update in validator_updates:
-            encoded_update = update.SerializeToString()
-            h.update(encoded_update)
-    
-    return h.hexdigest()
