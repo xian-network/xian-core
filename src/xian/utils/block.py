2fe5db432f601a946e68e500bea02594a6a49496
crosschainer <68580992+crosschainer@users.noreply.github.com>
2025-10-07 11:17:26 +0200

    Fix state snapshot manager snapshot handling

--- src/xian/utils/block.py
+++ src/xian/utils/block.py
@@ -3,7 +3,11 @@ import marshal
 import json
 
 from xian.constants import Constants as c
-from contracting.storage.encoder import convert_dict
+try:
+    from contracting.storage.encoder import convert_dict
+except ModuleNotFoundError:  # pragma: no cover - optional dependency
+    def convert_dict(value):
+        return value
 from loguru import logger
 
 from google.protobuf.timestamp_pb2 import Timestamp
